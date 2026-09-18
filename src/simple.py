3d33a971212d39caac3b2384177ef5e28d9f47d4
Jonathan Fisher 11 <JFisher29@cmc.edu>
2026-09-17 18:32:31 -0700

    Fixed function errors in python, test passing, p4

--- src/simple.py
+++ src/simple.py
@@ -7,8 +7,6 @@ and not for loops or list comprehensions.
 
 
 def evens(n):
-
-
     '''
     Returns a list of even numbers from 0 to n inclusive.
 
@@ -27,8 +25,6 @@ def evens(n):
 
 
 def threes(n):
-
-
     '''
     Returns a list of all numbers from 0 to n inclusive that contain 3.
 
@@ -43,12 +39,10 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    return list(filter(lambda x: '3' in str(x), range (n+1)))
+    return list(filter(lambda x: '3' in str(x), range(n+1)))
 
 
 def small_words(text):
-
-
     '''
     Returns a list of all words in the input text  less than 5 characters.
 
@@ -64,7 +58,8 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-    return list(filter(lambda x:  len(x)<5, text.split()))
+    return list(filter(lambda x:  len(x) < 5, text.split()))
+
 
 def squares(n):
     '''
@@ -81,6 +76,7 @@ def squares(n):
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
     return list(map(lambda x: x*x, range(1, n+1)))
+
 
 def lengths(strings):
     '''
